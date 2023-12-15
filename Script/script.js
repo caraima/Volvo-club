b43ecfeee939220d6df1de876f60c5c81040c309
Mihai <caraimanmishanea@gmail.com>
2023-12-15 13:22:48 +0200

    Adaugare galerie

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+
+let container = document.querySelectorAll('container')
+console.log(container);
+
+function incarcaFoto() {
+
+for (const container of container){
+    container.style.display ='block'
+}
+
+} 
